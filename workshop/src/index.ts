a5d2d7f82d8bc077c860ed8f0f16c68736038a3f
Antoine Kingue <etorefr@gmail.com>
2023-04-08 03:40:31 +0200

    🔥 remove async in the workshop

--- workshop/src/index.ts
+++ workshop/src/index.ts
@@ -1,10 +1,8 @@
 import dixt from "dixt";
 import dixtPluginLogs from "dixt-plugin-logs";
 
-(async () => {
-  const instance = new dixt({
-    plugins: [dixtPluginLogs],
-  });
+const instance = new dixt({
+  plugins: [dixtPluginLogs],
+});
 
-  await instance.start();
-})();
+instance.start();
