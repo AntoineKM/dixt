d7a485290d3b8bf822831b2e4065d02f98cacd55
Antoine Kingue <etorefr@gmail.com>
2023-05-12 21:55:03 +0200

    🔧 update worktime config

--- workshop/src/options/worktime.ts
+++ workshop/src/options/worktime.ts
@@ -5,10 +5,11 @@ import { CHANNELS, ROLES } from "../constants";
 const dixtPluginWorktimeOptions: DixtPluginWorktimeOptions = {
   channels: {
     leaderboard: CHANNELS.DIXT_PLUGIN_WORKTIME.LEADERBOARD,
-    main: [CHANNELS.DIXT_PLUGIN_WORKTIME.MAIN],
+    main: [CHANNELS.DIXT_PLUGIN_WORKTIME.MAIN, "1106660583366991973"],
     workChannelNames: ["work"],
   },
   quotas: {
+    [ROLES.MAINTAINER]: 2,
     [ROLES.CONTRIBUTOR]: 1,
   },
 };
