a19202ee26075bc60e4352736390b5fa2d6d8986
Antoine Kingue <etorefr@gmail.com>
2023-05-21 16:36:01 +0200

    🔧 update tsup config

--- packages/dixt/tsup.config.ts
+++ packages/dixt/tsup.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig, Options } from "tsup";
 
 const config: Options = {
-  entry: ["src/index.ts"],
-  splitting: false,
+  entry: ["src/**/*.ts"],
+  splitting: true,
   sourcemap: true,
   clean: true,
   platform: "node",
   dts: true,
+  minify: true,
 };
 
 export default defineConfig(config);
